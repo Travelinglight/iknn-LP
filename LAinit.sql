08ca2a3adce01f57c9aa51a09458dba5e36dc535
Kingston <3130000692@zju.edu.cn>
2015-07-18 19:51:33 +0800

    update lattices

--- LAinit.sql
+++ LAinit.sql
@@ -2,7 +2,7 @@ CREATE FUNCTION LAinit(tb text) RETURNS void
 AS $$
 DECLARE
     nColumn int;    -- the number of columns of the target table
-    vals text;   -- counting the number of incomplete attributes
+    vals text;   -- temp use
 BEGIN
 --  get the number of columns of the target table
     execute format('SELECT COUNT(*) FROM information_schema.columns WHERE table_name=%s;', quote_nullable(tb)) INTO nColumn;
@@ -30,6 +30,10 @@ BEGIN
     SELECT string_agg(format('CASE WHEN %s is null THEN ''0'' ELSE ''1'' END', column_name, column_name), ' || ') INTO vals FROM information_schema.columns WHERE ((table_name = ''||tb||'') and (column_name != 'alphavalue') and (column_name != 'nincomplete') and (column_name != 'ibitmap'));
     EXECUTE format('UPDATE %s SET ibitmap = (%s)', tb, vals);
 
+-- update the _LATMP table;
+    FOR vals IN EXECUTE format('SELECT distinct format(''INSERT INTO %s VALUES(%%s, %%s);'', nincomplete::text, quote_nullable(ibitmap)) FROM %s;', tb || '_LATMP', tb) LOOP
+        EXECUTE vals;
+    END LOOP;
 END
 $$
 language plpgsql;
