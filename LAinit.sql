eb70769142ae460bf305104fd36fdc211fc61116
Kingston <3130000692@zju.edu.cn>
2015-07-18 14:37:25 +0800

    LAinit simplified

--- LAinit.sql
+++ LAinit.sql
@@ -1,35 +1,20 @@
 CREATE FUNCTION LAinit(tb text) RETURNS void
 AS $$
 DECLARE
-    tmptb text; -- the tmp table name
     nColumn int;    -- the number of columns of the target table
-    cName text;  -- column name of the original table
-    getNC text; -- get nColumn;
-    addCalpha text; -- add column alphavalue to tb
-    addCl text; -- add column lattice key, the number of incomplete attributes, to tb
-    addCb text; -- add column bucket key, the bitmap of incomplete attributes, to tb
-    Ctb text;   -- Ctb is the command to create a table
-    tbhash text;    -- set up hash index on the created table
     vals text;   -- counting the number of incomplete attributes
 BEGIN
 --  get the number of columns of the target table
-    getNC := 'SELECT COUNT(*) FROM information_schema.columns WHERE table_name=' || quote_nullable(tb) || ';';
-    execute getNC INTO nColumn;
+    execute format('SELECT COUNT(*) FROM information_schema.columns WHERE table_name=%s;', quote_nullable(tb)) INTO nColumn;
 
 --  alter the target table
-    addCalpha := 'ALTER TABLE ' || tb || ' ADD COLUMN alphavalue float;';
-    addCl := 'ALTER TABLE ' || tb || ' ADD COLUMN nIncomplete int;';
-    addCb := 'ALTER TABLE ' || tb || ' ADD COLUMN Ibitmap varchar(' || nColumn::text  || ');';
-    execute addCalpha;
-    execute addCl;
-    execute addCb;
+    execute format('ALTER TABLE %s ADD COLUMN alphavalue float;', tb);
+    execute format('ALTER TABLE %s ADD COLUMN nIncomplete int;', tb);
+    execute format('ALTER TABLE %s ADD COLUMN Ibitmap varchar(%s);', tb, nColumn::text);
 
 --  create a temporary table for Lattices
-    tmptb := tb || '_LATMP';
-    Ctb := 'CREATE TABLE ' || tmptb || '(latticeID int, bucketID varchar(128));';
-    tbhash := 'CREATE INDEX tmphash ON ' || tmptb || ' USING HASH (latticeID);';
-    execute Ctb;
-    execute tbhash;
+    execute format('CREATE TABLE %s(latticeID int, bucketID varchar(%s));', tb || '_LATMP', nColumn::text);
+    execute format('CREATE INDEX tmphash ON %s USING HASH (latticeID);', tb || '_LATMP');
 
 --  update nIncomplete of the target table
     SELECT string_agg(format('(%s is null)::int',column_name), '+') INTO vals FROM information_schema.columns WHERE table_name = ''||tb||'';
