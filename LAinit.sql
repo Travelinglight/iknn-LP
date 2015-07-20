13b694bafa5763c08a832d0776164759f07e8242
Kingston <3130000692@zju.edu.cn>
2015-07-20 18:53:40 +0800

    update tmp table, and trigger for insertion/updating is added

--- LAinit.sql
+++ LAinit.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION hstore;
+
 CREATE FUNCTION LAinit(tb text) RETURNS void
 AS $$
 DECLARE
@@ -5,20 +7,19 @@ DECLARE
     vals text;   -- temp use
 BEGIN
 --  get the number of columns of the target table
-    execute format('SELECT COUNT(*) FROM information_schema.columns WHERE table_name=%s;', quote_nullable(tb)) INTO nColumn;
+    EXECUTE format('SELECT COUNT(*) FROM information_schema.columns WHERE table_name=%s;', quote_nullable(tb)) INTO nColumn;
 
 --  alter the target table
-    execute format('ALTER TABLE %s ADD COLUMN alphavalue float;', tb);
-    execute format('ALTER TABLE %s ADD COLUMN nIncomplete int;', tb);
-    execute format('ALTER TABLE %s ADD COLUMN Ibitmap varchar(%s);', tb, nColumn::text);
+    EXECUTE format('ALTER TABLE %s ADD COLUMN alphavalue float;', tb);
+    EXECUTE format('ALTER TABLE %s ADD COLUMN nIncomplete int;', tb);
+    EXECUTE format('ALTER TABLE %s ADD COLUMN Ibitmap varchar(%s);', tb, nColumn::text);
 
 --  create a temporary table for Lattices
-    execute format('CREATE TABLE %s(latticeID int, bucketID varchar(%s));', tb || '_LATMP', nColumn::text);
-    execute format('CREATE INDEX tmphash ON %s USING HASH (latticeID);', tb || '_LATMP');
+    EXECUTE format('CREATE TABLE %s(latticeID int, bucketID varchar(%s));', tb || '_LATMP', nColumn::text);
+    EXECUTE format('CREATE INDEX hash_%s ON %s USING HASH (latticeID);', tb || '_LATMP', tb || '_LATMP');
 
 --  update nIncomplete of the target table
-    SELECT string_agg(format('(%s is null)::int',column_name), '+') INTO vals FROM information_schema.columns WHERE table_name = ''||tb||'';
-    vals := vals || '-3';
+    SELECT string_agg(format('(%s is null)::int',column_name), '+') INTO vals FROM information_schema.columns WHERE ((table_name = ''||tb||'') and (column_name != 'alphavalue') and (column_name != 'nincomplete') and (column_name != 'ibitmap'));
     EXECUTE format('UPDATE %s SET nIncomplete = (%s)', tb, vals);
 
 -- update the alphavalue of the target table
@@ -30,10 +31,53 @@ BEGIN
     SELECT string_agg(format('CASE WHEN %s is null THEN ''0'' ELSE ''1'' END', column_name, column_name), ' || ') INTO vals FROM information_schema.columns WHERE ((table_name = ''||tb||'') and (column_name != 'alphavalue') and (column_name != 'nincomplete') and (column_name != 'ibitmap'));
     EXECUTE format('UPDATE %s SET ibitmap = (%s)', tb, vals);
 
+--  add hash index to the target table
+    EXECUTE format('CREATE INDEX hash_%s ON %s USING HASH (nincomplete);', tb, tb);
+
 -- update the _LATMP table;
     FOR vals IN EXECUTE format('SELECT distinct format(''INSERT INTO %s VALUES(%%s, %%s);'', nincomplete::text, quote_nullable(ibitmap)) FROM %s;', tb || '_LATMP', tb) LOOP
-        EXECUTE vals;
+    EXECUTE vals;
     END LOOP;
+
+-- create trigger for insertion/updating
+EXECUTE format('
+CREATE FUNCTION LP_%s_triInUp() RETURNS TRIGGER 
+AS $T2$
+DECLARE
+    r record;
+    nin int := 0;
+    nco int := 0;
+    alp float := 0;
+    bit text := '''';
+BEGIN
+-- compute the three additional attributes
+    FOR r IN SELECT (each(hstore(NEW))).*
+    LOOP
+        IF r.key = ''alphavalue'' THEN CONTINUE; END IF;
+        IF r.key = ''nincomplete'' THEN CONTINUE; END IF;
+        IF r.key = ''ibitmap'' THEN CONTINUE; END IF;
+        IF r.value is null THEN
+            nin := nin + 1;
+            bit := bit || ''0'';
+        ELSE
+            nco := nco + 1;
+            alp := alp + r.value::int;
+            bit := bit || ''1'';
+        END IF;
+    END LOOP;
+    alp := alp / nco;
+    NEW.nincomplete := nin;
+    NEW.alphavalue := alp;
+    NEW.ibitmap := bit;
+    
+-- update lattice table
+    EXECUTE format(''INSERT INTO %s_latmp SELECT %%s, %%s WHERE NOT EXISTS ( SELECT * FROM hash_latmp WHERE latticeid = %%s and bucketid = %%s);'', nin, quote_nullable(bit), nin, quote_nullable(bit));
+    RETURN NEW;
+END
+$T2$ LANGUAGE plpgsql;', tb, tb);
+EXECUTE format('CREATE TRIGGER %s_LAinup BEFORE INSERT OR UPDATE ON %s
+FOR EACH ROW EXECUTE PROCEDURE LP_%s_triInUp();', tb, tb, tb);
+
 END
 $$
 language plpgsql;
--- LAwithdraw.sql
+++ LAwithdraw.sql
@@ -1,28 +1,26 @@
 CREATE FUNCTION LAwithdraw(tb text) RETURNS void
 AS $$
-DECLARE
-    tmptb text; -- the tmp table name
-    delCalpha text; -- delete column alphavalue from tb
-    delCl text; -- delelete column lattice key, the number of incomplete attributes, from tb
-    delCb text; -- delelete column bucket key, the bitmap of incomplete attributes, from tb
-    Dtb text;   -- Ctb is the command to delete the temporary table
---    selectALL text; -- select all tuples from the target table for updating
 BEGIN
---  alter the target table
-    delCalpha := 'ALTER TABLE ' || tb || ' DROP COLUMN alphavalue;';
-    delCl := 'ALTER TABLE ' || tb || ' DROP COLUMN nIncomplete;';
-    delCb := 'ALTER TABLE ' || tb || ' DROP COLUMN Ibitmap;';
-    execute delCalpha;
-    execute delCl;
-    execute delCb;
+--  delete the extra 3 columns of the target table
+    EXECUTE format('ALTER TABLE %s DROP COLUMN alphavalue;', tb);
+    EXECUTE format('ALTER TABLE %s DROP COLUMN nIncomplete;', tb);
+    EXECUTE format('ALTER TABLE %s DROP COLUMN Ibitmap;', tb);
 
---  create a temporary table for Lattices
-    tmptb := tb || '_LATMP';
-    Dtb := 'DROP TABLE ' || tmptb;
-    execute Dtb;
+--  drop the hash index on target table
+    EXECUTE format('DROP INDEX hash_%s', tb);
+
+--  drop the temporary table for Lattices
+    EXECUTE format('DROP TABLE %s;', tb || '_LATMP');
+
+--  drop triggers
+    EXECUTE format('DROP TRIGGER %s_lainup ON %s', tb, tb);
+    EXECUTE format('DROP FUNCTION lp_%s_triinup();', tb);
 
 --  drop function (for debugging)
-    execute 'DROP FUNCTION lainit(text);';
+    EXECUTE 'DROP FUNCTION lainit(text);';
+
+--  drop extension (for debugging)
+    DROP EXTENSION hstore;
 END
 $$
 language plpgsql;
